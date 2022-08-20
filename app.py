125ade1f433d210ac47747547ebc992340513669
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2022-08-20 10:46:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! My name is ashok"
+#     return "Hello World! My name is ashok"
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
