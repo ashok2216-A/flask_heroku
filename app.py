fc556f6b50f166243a65d1ea43a20071080c590a
ashok_2216 <ashoksiva2216@gmail.com>
2022-08-26 11:16:01 +0530

    code added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-     return "Hello World! My name is ashok"
+     # return "Hello World! My name is ashok"
+     render_template('template\index.html')
 
 if __name__ == "__main__":
     app.run()
