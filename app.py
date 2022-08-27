e67679ac2b230dbfcbf2eae142ad15fa27b4696d
ashok_2216 <ashoksiva2216@gmail.com>
2022-08-27 10:18:26 +0530

    file added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-     return "Hello World! My name is ashok"
-     
+@app.route("/<name>")
+def home(name):
+     # return "Hello World! My name is ashok"
+     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
