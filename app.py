7f9b83ab1af4288bbf47e82165d4c2b24ba5dd7f
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2022-09-03 19:13:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,11 @@
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route("/")
-# def home():
-#      # return "Hello World! My name is ashok"
-#      return render_template('index.html')
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
-
+from flask import Flask, render_template
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
-
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    features = [int(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 1)
-
-    return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
+     # return "Hello World! My name is ashok"
+     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
